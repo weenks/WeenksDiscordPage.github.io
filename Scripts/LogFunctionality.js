05afc30db5c755572ee591627f45c45c0474e4ba
xChromaz <xchromaz35@gmail.com>
2021-04-23 13:21:47 -0500

    LogIn Functionality

--- Scripts/LogFunctionality.js
+++ Scripts/LogFunctionality.js
@@ -1,3 +1,11 @@
 function linkHtmlPage(){
-    window.location.href="Home.html";
+    let inEma = document.getElementById("password").value
+    let inPass = document.getElementById("email").value
+    if(inEma == "admin" && inPass == "admin"){
+        window.location.href="Home.html";
+    }
+    else{
+        alert("Invalid email and password")
+    }
+    
 }
