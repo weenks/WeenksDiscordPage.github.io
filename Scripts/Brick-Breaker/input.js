9d34663a867039a86ca5e499a53058427927e0a7
xChromaz <xchromaz35@gmail.com>
2021-05-12 18:52:06 -0500

    Test 2

--- Scripts/Brick-Breaker/input.js
+++ Scripts/Brick-Breaker/input.js
@@ -1,44 +1,35 @@
-export default class InputHandler{
-    constructor(paddle){
-        document.addEventListener("keydown", event => {
-            switch (event.keyCode) {
-                case 37:
-                    paddle.moveLeft();
-                    break;
+export default class InputHandler {
+  constructor(paddle, game) {
+    document.addEventListener("keydown", event => {
+      switch (event.keyCode) {
+        case 37:
+          paddle.moveLeft();
+          break;
 
-                case 39:
-                    paddle.moveRight();
-                    break;
-                
-                case 65:
-                    paddle.moveLeft();
-                    break;
-                
-                case 68:
-                    paddle.moveRight();
-                    break;
-            }
-        });
+        case 39:
+          paddle.moveRight();
+          break;
 
-        document.addEventListener("keyup", event => {
-            switch (event.keyCode) {
-                case 37:
-                    if(paddle.speed < 0) paddle.stop();
-                    break;
+        case 27:
+          game.togglePause();
+          break;
 
-                case 39:
-                    if(paddle.speed > 0) paddle.stop();
-                    break;
-                
-                case 65:
-                    if(paddle.speed < 0) paddle.stop();
-                    break;
-                
-                case 68:
-                    if(paddle.speed > 0) paddle.stop();
-                    break;
-            }
-        });
-    }
+        case 32:
+          game.start();
+          break;
+      }
+    });
+
+    document.addEventListener("keyup", event => {
+      switch (event.keyCode) {
+        case 37:
+          if (paddle.speed < 0) paddle.stop();
+          break;
+
+        case 39:
+          if (paddle.speed > 0) paddle.stop();
+          break;
+      }
+    });
+  }
 }
-
--- Scripts/Brick-Breaker/paddle.js
+++ Scripts/Brick-Breaker/paddle.js
@@ -1,39 +1,42 @@
 export default class Paddle {
-    constructor(gameWidth, gameHeight){
-        this.gameWidth = gameWidth;
-        this.width = 150;
-        this.height = 30;
-        this.maxSpeed = 6;
-        this.speed = 0;
-        this.position = {
-            x: gameWidth / 2 - this.width / 2,
-            y: gameHeight - this.height - 10
-        };
-    }
+  constructor(game) {
+    this.gameWidth = game.gameWidth;
 
-    moveLeft(){
-        this.speed = -this.maxSpeed;
-    }
+    this.width = 150;
+    this.height = 20;
 
-    moveRight(){
-        this.speed = this.maxSpeed;
-    }
+    this.maxSpeed = 7;
+    this.speed = 0;
 
-    stop(){
-        this.speed = 0;
-    }
+    this.position = {
+      x: game.gameWidth / 2 - this.width / 2,
+      y: game.gameHeight - this.height - 10
+    };
+  }
 
-    draw(ctx) {
-        ctx.fillStyle = "#0ff";
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
-    }
+  moveLeft() {
+    this.speed = -this.maxSpeed;
+  }
 
-    update(deltaTime){
-        if(!deltaTime) return;
+  moveRight() {
+    this.speed = this.maxSpeed;
+  }
 
-        this.position.x += this.speed;
-        if(this.position.x < 0) this.position.x = 0;
-        if(this.position.x + this.width > this.gameWidth)
-            this.position.x = this.gameWidth - this.width;
-    }
+  stop() {
+    this.speed = 0;
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = "#0ff";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    this.position.x += this.speed;
+
+    if (this.position.x < 0) this.position.x = 0;
+
+    if (this.position.x + this.width > this.gameWidth)
+      this.position.x = this.gameWidth - this.width;
+  }
 }
